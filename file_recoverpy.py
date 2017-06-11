276d5525facfbf520936f95fd1132ead6b3ba291
Virgilio <virgilio.castro@bec.seguridad.unam.mx>
2017-06-10 22:20:11 -0400

    Commented

--- file_recoverpy.py
+++ file_recoverpy.py
@@ -48,52 +48,54 @@ def addOptions():
 def writeFile(ftype,number,data):
 	with open('recovery/%s/file%s.%s' % (ftype,number,ftype),'wb') as out:
 		out.write(data)
+		print 'File file%s.%s has been extracted!' % (number,ftype)
 
 
 #Busca los diferentes tipos de archivos en la imagen
 def findFiles(image,formats,limit):
 	with open(image,'rb') as im_f:
-		c_header = 0 #Contador para validar que se cubre correctamente el header
-		c_footer = 0 #Contador para validar que se leyó correctamente el footer
-		c_file = 0 #Contador de archivos extraidos
-		file = '' #Cadena del archivo a extraer
 		for f in formats:
-			im_f.seek(0,0)
-			header = headers[f]
+			c_file = 0	#Contador de archivos extraidos
+			c_header = 0	#Contador para validar que se cubre correctamente el header
+			c_footer = 0	#Contador para validar que se leyó correctamente el footer
+			file = ''	#Cadena del archivo a extraer
+			im_f.seek(0,0)	#Por cada formato, vuelve a leer el archivo desde el inicio
+			header = headers[f]	#Almacena header y footer del formato actual
 			footer = footers[f]
 			while True:
-				byte = str(im_f.read(1))
-				if not byte: break
-				if c_header < len(header):
-					if (byte == header[c_header].decode('hex')):
-						c_header += 1
+				byte = str(im_f.read(1))	#Lee un byte y lo convierte a cadena para su manejo
+				if not byte: break		#Rompe el ciclo si llega al final del archivo
+				if c_header < len(header):	#Valida si el contador de la cabecera aún no llega al tamaño esperado
+					if (byte == header[c_header].decode('hex')):	#Decodifica el byte de la cabecera actual y lo compara con el leido
+						c_header += 1	#Si son iguales aumenta el contador y agrega el byte al archivo
 						file += byte
-					else:
+					else:	#Si no se cumple la cabecera compeltamente, resetea contador y vacía archivo
 						c_header = 0
 						file = ''
-				if c_header == len(header):
-					print file
-					while True:
+				if c_header == len(header): #Entra aquí sólo si encontró la cabecera completa
+					while True:	#A partir de aquí busca el resto del archivo
 						byte = str(im_f.read(1))
 						if not byte: break
-						file += byte
+						file += byte	#Lee un byte por vez y lo va agregando al archivo
 						if footer is None:
-							if len(file) > limit: break
-						elif c_footer < len(footer):
+							if len(file) > limit: break	#Rompe el ciclo si no hay un footer definido
+						elif c_footer < len(footer):	#Valida que se encuentre el footer completo
 							if (byte == footer[c_footer].decode('hex')):
 								c_footer += 1
 							else:
 								c_footer = 0
-							if c_footer == len(footer): break
-					writeFile(f,c_file,file)
-					c_file += 1
-					c_header = 0
+							if c_footer == len(footer): break #Rompe el ciclo
+					writeFile(f,c_file,file) #Escribe la cadena encontrada
+					c_file += 1 #Como encontró un archivo, aumenta el contador
+					c_header = 0 #Resetea valores para el siguiente archivo
 					c_footer = 0
 					file = ''
+
 
 #Inicio del programa
 if __name__ == '__main__':
 	try:
+		print '\t\tGNU File recoverpy V1.0'
 		opts = addOptions() #Agrega opciones
 		if not os.path.exists('recovery'): #Crea carpetas necesarias para almacenar archvos recuperados
 			os.makedirs('recovery')
