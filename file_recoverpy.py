54b809bd7bdcc97056b47f3771a9ee897f5cef5a
Virgilio <virgilio.castro@bec.seguridad.unam.mx>
2017-06-10 21:54:24 -0400

    Limit implemented

--- file_recoverpy.py
+++ file_recoverpy.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 #Castro Rendón Virgilio
 #File Recoverpy v1.0
-from datetime import datetime
+from datetime import datetime #Será útil cuando se implemente la generación de reportes
 import sys
 import optparse
-import struct
 import os
 
+
+#Cabeceras y "pies" de los diferentes tipos de archivo
+#Si no tiene un footer, es nulo y el tamaño se determinará por un límite arbitrario
 headers = {'jpg':['ff','d8','ff'],
 	'jpeg':['ff','d8','ff'],
 	'png':['89','50','4e','47'],
@@ -33,7 +35,7 @@ def addOptions():
 	parser = optparse.OptionParser()
 	parser.add_option('-i','--input', dest='input', default=None, help='Forensic image to analyse')
 	parser.add_option('-f','--format', dest='format', default='jpg,gif,png,pdf', help='File format to look for')
-	parser.add_option('-l','--limit', dest='limit', default='1048576', help='Number of bytes per file. If 0, will look for footer')
+	parser.add_option('-l','--limit', dest='limit', default='1048576', help='Number of bytes per file. (If ft doesn\'t have a footer') #Límite si no hay footer. 1MB
 	opts,args = parser.parse_args()
 	if opts.input is None:		#Forzosamente se debe indicar una imagen forense
 		printError('Specify an image to process')
@@ -41,17 +43,20 @@ def addOptions():
 		if f not in ['jpg','jpeg','gif','png','pdf']: printError('Format not supported (%s)' % f)
 	return opts
 
+
+#Escribe los archivos en las respectivas carpetas, con nombre secuencial
 def writeFile(ftype,number,data):
 	with open('recovery/%s/file%s.%s' % (ftype,number,ftype),'wb') as out:
 		out.write(data)
 
+
 #Busca los diferentes tipos de archivos en la imagen
-def findFiles(image,formats):
+def findFiles(image,formats,limit):
 	with open(image,'rb') as im_f:
-		c_header = 0
-		c_footer = 0
-		c_file = 0
-		file = ''
+		c_header = 0 #Contador para validar que se cubre correctamente el header
+		c_footer = 0 #Contador para validar que se leyó correctamente el footer
+		c_file = 0 #Contador de archivos extraidos
+		file = '' #Cadena del archivo a extraer
 		for f in formats:
 			im_f.seek(0,0)
 			header = headers[f]
@@ -72,7 +77,9 @@ def findFiles(image,formats):
 						byte = str(im_f.read(1))
 						if not byte: break
 						file += byte
-						if c_footer < len(footer):
+						if footer is None:
+							if len(file) > limit: break
+						elif c_footer < len(footer):
 							if (byte == footer[c_footer].decode('hex')):
 								c_footer += 1
 							else:
@@ -88,14 +95,14 @@ def findFiles(image,formats):
 if __name__ == '__main__':
 	try:
 		opts = addOptions() #Agrega opciones
-		if not os.path.exists('recovery'):
+		if not os.path.exists('recovery'): #Crea carpetas necesarias para almacenar archvos recuperados
 			os.makedirs('recovery')
 		formats = opts.format.split(',')
 		for format in formats:
 			if not os.path.exists('recovery/%s' % format):
 				os.makedirs('recovery/%s' % format)
-		findFiles(opts.input,formats)
+		findFiles(opts.input,formats,int(opts.limit))
 	except IOError:
 		printError('File does not exist')
-	except Error:
+	except Exception:
 		printError('Unexpected error')
